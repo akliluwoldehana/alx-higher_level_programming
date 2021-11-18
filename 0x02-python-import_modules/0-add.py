40bf0ef60ebdfa5848212fef83d4088660c2fff0
HP <akliluwoldehana@gmail.com>
2021-11-18 15:53:21 +0300

    Task one

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def add(a, b):
-    a=1
-    b=2
     return a + b
-if(__name__ == 'main'):
-    add()
+if(__name__ == '__main__'):
+    print("{} + {} = {}".format(1,2,add(1,2)))
